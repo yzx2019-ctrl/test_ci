db2538a5d54424bbe244da737898d201302c3060
sonxiao <58676802+yzx2019-ctrl@users.noreply.github.com>
2021-09-08 23:36:19 +0800

    first commit

---
+++ scripts/test01_demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+import time
+
+class TestDemo(object):
+
+    def setup_class(self):
+        print("开始")
+        self.driver = webdriver.Chrome()
+
+    def teardown_class(self):
+        self.driver.quit()
+        print("结束")
+
+    def test_open(self):
+        self.driver.get("http://www.baidu.com")
+        self.driver.find_element_by_id("kw").send_keys("宋春梅")
+        self.driver.find_element_by_id("su").click()
+        time.sleep(5)
+
+    def test_hah(self):
+        print("hahah")
